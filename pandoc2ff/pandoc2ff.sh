f02d1fe718ed5ca7ebb61ff5288f5b4db9a24dcd
Bjoern <ugfam@student.kit.edu>
2017-09-05 13:30:06 +0200

    bugfix with working dir. and addad params fo pandoc

--- pandoc2ff/pandoc2ff.sh
+++ pandoc2ff/pandoc2ff.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 set -e
-TARGET=$1
+TARGET="$*"
 BASEDIR=$(dirname ${BASH_SOURCE[0]})"/"
 OUTFILE=$(mktemp -d -t)"/o.html"
 CSSFILE=$BASEDIR"pandoc.css"
-PANDOCCMD="pandoc -s --mathjax --self-contained --latex-engine=xelatex -f markdown_github -o $OUTFILE --css $CSSFILE"
+PANDOCCMD="pandoc --mathjax --smart --standalone --normalize --self-contained --write=html5 --latex-engine=xelatex -f markdown_github -o $OUTFILE --css $CSSFILE"
 
 if [ -d "$TARGET" ]; then
-  cd  $TARGET
+  cd  "$TARGET"
   cat *.md| $PANDOCCMD
 else
-  $PANDOCCMD $TARGET
+  cd "$(dirname "$(readlink -f "$TARGET")")"
+  $PANDOCCMD $(basename "$TARGET")
 fi
 firefox $OUTFILE
+sleep 10
 rm $OUTFILE
